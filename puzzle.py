6921f9cc53ed998a5336f445b24d6f75f19465d6
Andrew Frantz <fraam-19@rhodes.edu>
2018-03-08 01:13:32 -0500

    Updated remove_candidates

--- puzzle.py
+++ puzzle.py
@@ -6,8 +6,10 @@ class Cell:
         else:
             self.candidates = {val for val in range(1, 10)}
 
-    def remove_candidates(self, candidates):
-        self.candidates.remove(candidates)
+    def remove_candidate(self, candidate):
+        if candidate in self.candidates:
+            self.candidates.remove(candidate)
+            self.print_cell()
 
     def print_cell(self):
         print('Cell: ({}, {})'.format(self.POS[0], self.POS[1]))
@@ -16,18 +18,18 @@ class Cell:
 
 class Puzzle:
     def __init__(self, raw_puzzle):
-        self.candidates_array = []
+        self.cell_array = []
         pos = 0
         for row in range(9):
-            self.candidates_array.append([])
+            self.cell_array.append([])
             for col in range(9):
                 if raw_puzzle[pos] != '.':
-                    self.candidates_array[row].append(Cell(row, col, int(raw_puzzle[pos])))
+                    self.cell_array[row].append(Cell(row, col, int(raw_puzzle[pos])))
                 else:
-                    self.candidates_array[row].append(Cell(row, col))
+                    self.cell_array[row].append(Cell(row, col))
                 pos += 1
 
     def print_all_candidates(self):
         for row in range(9):
             for col in range(9):
-                self.candidates_array[row][col].print_cell()
+                self.cell_array[row][col].print_cell()
