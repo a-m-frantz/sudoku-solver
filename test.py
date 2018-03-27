00763db7078c44710d91f82792615643a6d41f2d
Andrew Frantz <fraam-19@rhodes.edu>
2018-03-27 14:39:56 -0400

    Added total, min, max, and stddev calculations to test.py

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import sudokuSolver
 import time
+import statistics
 from contextlib import contextmanager
 import sys
 import os
@@ -17,10 +18,11 @@ def suppress_stdout():
 
 
 def test():
-    num_tests = 10
-    print('#############################################################')
-    print('Timing sudokuSolver over {} runs.'.format(num_tests))
-    print('#############################################################', end='\n\n')
+    num_tests = 100
+    print('####################################')
+    print('# Timing sudokuSolver over {} runs #'.format(num_tests))
+    print('####################################', end='\n\n')
+    start_time = time.time()
     run_times = []
     with suppress_stdout():
         for _ in range(num_tests):
@@ -28,8 +30,20 @@ def test():
             sudokuSolver.main()
             t_end = time.time()
             run_times.append(t_end - t_start)
+    end_time = time.time()
+
+    total_time = end_time - start_time
+    max_time = max(run_times)
+    min_time = min(run_times)
     avg_time = sum(run_times) / len(run_times)
-    print('Average runtime = {}'.format(avg_time))
+    std_dev = statistics.stdev(run_times)
+    print('Total time = {0:.4f}'.format(total_time))
+    print()
+    print('Maximum runtime = {0:.4f}'.format(max_time))
+    print('Minimum runtime = {0:.4f}'.format(min_time))
+    print()
+    print('Average runtime = {0:.4f}'.format(avg_time))
+    print('Standard Deviation = {0:.4f}'.format(std_dev))
 
 
 if __name__ == '__main__':
