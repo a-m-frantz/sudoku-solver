379a273d26f44611ca221fc17cbc900ce5fd3425
Andrew Frantz <fraam-19@rhodes.edu>
2018-03-30 12:43:20 -0400

    Broke read_file() into two functions, read_file() and parse_file().

--- sudokuSolver.py
+++ sudokuSolver.py
@@ -5,28 +5,38 @@ import algorithm as alg
 import puzzle as pzl
 
 
+def parse_file(file_contents):
+    """Validate that file has a puzzle with at least 17 clues and return the puzzle in string format if validated."""
+    puzzle_list = [char for char in file_contents if char.isdigit or char == '.']
+    puzzle_string = ''.join(puzzle_list)
+    if len(puzzle_string) == 81:
+        clues = [char for char in puzzle_string if char != '.' and char != '0']
+        num_clues = len(clues)
+        if num_clues >= 17:
+            return puzzle_string
+        else:
+            print('This is an unsolvable puzzle. It has {} clues.\n'
+                  'There are no valid sudoku puzzles with fewer than 17 clues.'.format(num_clues))
+            return None
+    else:
+        print('File in incorrect format.\nSee README for accepted puzzle formats.')
+        return None
+
+
 def read_file():
-    """Get puzzle file from user, then validate it and create a Puzzle object."""
+    """Get puzzle file from user, validate it, and return a Puzzle object."""
     print('Type "exit" at the prompt to quit.')
     while True:
         try:
             infile_name = input('Puzzle file name: ')
-            # infile_name = 'hard3.txt'
+            # infile_name = 'hardestMultipleSolutions.txt'
             if infile_name == 'exit':
                 sys.exit('User quit program.')
             infile = open(infile_name)
             file_contents = infile.read()
-            puzzle_list = [char for char in file_contents if char.isdigit or char == '.']
-            puzzle_string = ''.join(puzzle_list)
-            if len(puzzle_string) == 81:
-                clues = [char for char in puzzle_string if char != '.' and char != '0']
-                num_clues = len(clues)
-                if num_clues >= 17:
-                    break
-                print('{} is an unsolvable puzzle. It has {} clues.\n'
-                      'There are no valid sudoku puzzles with fewer than 17 clues.'.format(infile_name, num_clues))
-            else:
-                print('File in incorrect format.\nSee README for accepted puzzle formats.')
+            puzzle_string = parse_file(file_contents)
+            if puzzle_string:
+                break
         except OSError:
             print('File not found. Please try again.')
     print('Input file: ' + infile_name, end='\n\n')
